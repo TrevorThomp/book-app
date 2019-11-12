44ff4c1aab1fd11cec0b5e7e350f5d163e5fd716
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2019-11-12 09:15:31 -0800

    completed url to google Books API and mapping over data

--- data/books.js
+++ data/books.js
@@ -1,7 +1,28 @@
 'use strict';
 
+const superagent = require('superagent');
+
 function Book(info) {
-  this.title = info.title || 'No title available';
-  this.author = this.author || 'No author available';
-  this.description = this.description || 'No description available';
+  this.title = info.volumeInfo.title || 'No title available';
+  this.author = this.volumeInfo.authors || 'No author available';
+  this.description = this.volumeInfo.description || 'No description available';
 }
+
+
+function getBooks(request,response) {
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${request.query.data}`;
+
+  return superagent
+    .get(url)
+    .then(result => {
+      console.log(result)
+      let bookData = request.query.data.items.map(data => {
+        return new Book(data)
+      })
+      response.status(200).json(bookData)
+    }
+    )
+    .catch(() => console.log('Something is not right'), request,response)
+}
+
+module.export = getBooks;
