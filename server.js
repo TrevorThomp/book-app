ceba7c4dce6a73871ec16ff3ccd9287ec344d944
TrevorThomp <trevor.ray.thompson@gmail.com>
2019-11-11 19:42:16 -0800

    initial server structure and base proof styling

--- server.js
+++ server.js
@@ -12,9 +12,12 @@ app.use(cors());
 app.use(express.static('public'));
 
 // View Engine
+app.set('views', './views');
 app.set('view engine', 'ejs');
 
-
+app.get('/', (request,response) => {
+  response.render('index')
+});
 
 
 app.listen(PORT, console.log(`Listening on ${PORT}`));
